6defd29d13b6105a750981b352d1961a301197a3
doug157 <73317004+doug157@users.noreply.github.com>
2021-01-04 17:14:24 -0500

    Update includes.h

--- includes.h
+++ includes.h
@@ -10,16 +10,16 @@
 #include <mutex>
 #include "apex.h"
 
-#define ENTITY_LIST			0x18c7af8
-#define ENTITY_TYPE			0x560
-#define CLIENT_STATE		0x127c0e0
-#define SIGNON_STATE		0x127c178
-#define LEVEL_NAME			0x127c290
-#define GLOW				0x3C0
-#define GLOW_DURATION		0x310
-#define GLOW_RANGE			0x33C
-#define HIGHLIGHT_CONTEXT	0x350
-#define COLOR				0x1D0
+#define ENTITY_LIST 0x18c7af8
+#define ENTITY_TYPE 0x560
+#define CLIENT_STATE 0x127c0e0
+#define SIGNON_STATE 0x127c178
+#define LEVEL_NAME 0x127c290
+#define GLOW 0x3C0
+#define GLOW_DURATION 0x310
+#define GLOW_RANGE 0x33C
+#define HIGHLIGHT_CONTEXT 0x350
+#define COLOR 0x1D0
 
-#define BRIGHTNESS			0.1f
-#define SRGB				RGB(195, 0, 220)
+#define BRIGHTNESS 0.1f
+#define SRGB RGB(195, 0, 220)
